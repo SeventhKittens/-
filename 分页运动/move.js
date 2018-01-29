2bf277a65b39b22d664126afee69528538cb5b65
SeventhKittens <35600955+SeventhKittens@users.noreply.github.com>
2018-01-30 00:44:05 +0800

    Create move.js

---
+++ 分页运动/move.js
@@ -0,0 +1,195 @@
+// JavaScript Document
+
+function startMove(obj,json,endFn){
+	
+		clearInterval(obj.timer);
+		
+		obj.timer = setInterval(function(){
+			
+			var bBtn = true;
+			
+			for(var attr in json){
+				
+				var iCur = 0;
+			
+				if(attr == 'opacity'){
+					if(Math.round(parseFloat(getStyle(obj,attr))*100)==0){
+					iCur = Math.round(parseFloat(getStyle(obj,attr))*100);
+					
+					}
+					else{
+						iCur = Math.round(parseFloat(getStyle(obj,attr))*100) || 100;
+					}	
+				}
+				else{
+					iCur = parseInt(getStyle(obj,attr)) || 0;
+				}
+				
+				var iSpeed = (json[attr] - iCur)/3;
+			iSpeed = iSpeed >0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+				if(iCur!=json[attr]){
+					bBtn = false;
+				}
+				
+				if(attr == 'opacity'){
+					obj.style.filter = 'alpha(opacity=' +(iCur + iSpeed)+ ')';
+					obj.style.opacity = (iCur + iSpeed)/100;
+					
+				}
+				else{
+					obj.style[attr] = iCur + iSpeed + 'px';
+				}
+				
+				
+			}
+			
+			if(bBtn){
+				clearInterval(obj.timer);
+				
+				if(endFn){
+					endFn.call(obj);
+				}
+			}
+			
+		},30);
+	
+	}
+	
+	
+	function getStyle(obj,attr){
+		if(obj.currentStyle){
+			return obj.currentStyle[attr];
+		}
+		else{
+			return getComputedStyle(obj,false)[attr];
+		}
+	}
+	function page(opt){
+
+	if(!opt.id){return false};
+	
+	var obj = document.getElementById(opt.id);
+	
+	var nowNum = opt.nowNum || 1;
+	var allNum = opt.allNum || 5;
+	var callBack = opt.callBack || function(){};
+	
+	if( nowNum>=4 && allNum>=6 ){
+	
+		var oA = document.createElement('a');
+		oA.href = '#1';
+		oA.innerHTML = '首页';
+		obj.appendChild(oA);
+	
+	}
+	
+	if(nowNum>=2){
+		var oA = document.createElement('a');
+		oA.href = '#' + (nowNum - 1);
+		oA.innerHTML = '上一页';
+		obj.appendChild(oA);
+	}
+	
+	if(allNum<=5){
+		for(var i=1;i<=allNum;i++){
+			var oA = document.createElement('a');
+			oA.href = '#' + i;
+			if(nowNum == i){
+				oA.innerHTML = i;
+			}
+			else{
+				oA.innerHTML = '['+ i +']';
+			}
+			obj.appendChild(oA);
+		}	
+	}
+	else{
+	
+		for(var i=1;i<=5;i++){
+			var oA = document.createElement('a');
+			
+			
+			if(nowNum == 1 || nowNum == 2){
+				
+				oA.href = '#' + i;
+				if(nowNum == i){
+					oA.innerHTML = i;
+				}
+				else{
+					oA.innerHTML = '['+ i +']';
+				}
+				
+			}
+			else if( (allNum - nowNum) == 0 || (allNum - nowNum) == 1 ){
+			
+				oA.href = '#' + (allNum - 5 + i);
+				
+				if((allNum - nowNum) == 0 && i==5){
+					oA.innerHTML = (allNum - 5 + i);
+				}
+				else if((allNum - nowNum) == 1 && i==4){
+					oA.innerHTML = (allNum - 5 + i);
+				}
+				else{
+					oA.innerHTML = '['+ (allNum - 5 + i) +']';
+				}
+			
+			}
+			else{
+				oA.href = '#' + (nowNum - 3 + i);
+				
+				if(i==3){
+					oA.innerHTML = (nowNum - 3 + i);
+				}
+				else{
+					oA.innerHTML = '['+ (nowNum - 3 + i) +']';
+				}
+			}
+			obj.appendChild(oA);
+			
+		}
+	
+	}
+	
+	if( (allNum - nowNum) >= 1 ){
+		var oA = document.createElement('a');
+		oA.href = '#' + (nowNum + 1);
+		oA.innerHTML = '下一页';
+		obj.appendChild(oA);
+	}
+	
+	if( (allNum - nowNum) >= 3 && allNum>=6 ){
+	
+		var oA = document.createElement('a');
+		oA.href = '#' + allNum;
+		oA.innerHTML = '尾页';
+		obj.appendChild(oA);
+	
+	}
+	
+	callBack(nowNum,allNum);
+	
+	var aA = obj.getElementsByTagName('a');
+	
+	for(var i=0;i<aA.length;i++){
+		aA[i].onclick = function(){
+			
+			var nowNum = parseInt(this.getAttribute('href').substring(1));
+			
+			obj.innerHTML = '';
+			
+			page({
+			
+				id : opt.id,
+				nowNum : nowNum,
+				allNum : allNum,
+				callBack : callBack
+			
+			});
+			
+			return false;
+			
+		};
+	}
+
+}
